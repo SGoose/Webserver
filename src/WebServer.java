39ed1b42f8e5254a60e579db8c6cbfd3a111bb8c
SGoose <dragonandnaruto@gmail.com>
2022-10-03 20:29:05 +0300

    Initial commit

---
+++ src/WebServer.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class WebServer {
+
+    public static void main(String[] args) {
+        try (ServerSocket serverSocket = new ServerSocket(8080)) {
+            System.out.println("Server started!");
+
+            while (true) {
+                Socket socket = serverSocket.accept();
+                System.out.println("New client connected!");
+
+                new Thread(() -> Handler.handleRequest(socket)).start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
